b60fd51f71b9f249a1ea66294b2a76984b0e1037
Lara <Laraewellen@gmail.com>
2026-03-04 10:49:57 -0300

    Adiciona alerta sonoro com winsound à simulação de relógio em Python

--- exemplo_relogio.py
+++ exemplo_relogio.py
@@ -1,8 +1,12 @@
 import time
 import os
-for h in range (1, 24):
+import winsound
+for h in range(1, 24):
     for m in range(1, 60):
         for s in range(1, 60):
             print("h: ", h, "m: ", m, "s: ", s)
             time.sleep(1)
             os.system("cls")
+            frequency = 2500  # Set Frequency To 2500 Hertz
+            duration = 1000  # Set Duration To 1000 ms == 1 second
+            winsound.Beep(frequency, duration)
