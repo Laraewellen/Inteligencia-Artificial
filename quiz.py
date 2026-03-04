e0e95f7f38e095e9d48fed909a77bd4fc8e2c589
Lara <Laraewellen@gmail.com>
2026-03-04 11:15:37 -0300

    quiz

--- quiz.py
+++ quiz.py
@@ -8,7 +8,48 @@ perguntas = {
         'pergunta': 'Qual o Professor mais legal?',
         'respostas': {'a': 'Joao', 'b': 'Pedro', 'c': 'Maria', },
         'resposta_certa': 'a',
-    }
+    },
+    'Pergunta 3' : {
+        'pergunta': 'Qual a melhor cor?',
+        'respostas': {'a': 'azul', 'b': 'rosa', 'c': 'roxo',},
+        'resposta_certa': 'c'
+    },
+    'Pergunta 4': {
+        'pergunta': 'complete a frase "Eu sou o ..."',
+        'respostas': {'a': 'superman', 'b': 'batman', 'c': 'coringa'},
+        'resposta_certa': 'b'
+    },
+    'Pergunta 5': {
+        'pergunta': 'Com que a Zendaya se casou?"',
+        'respostas': {'a': 'Tom Holland', 'b': 'Timothe', 'c': 'Glen'},
+        'resposta_certa': 'a'
+    },
+    'Pergunta 6': {
+        'pergunta': 'Qual a profissão do clark kent',
+        'respostas': {'a': 'reporter', 'b': 'fotografo', 'c': 'influencer'},
+        'resposta_certa': 'a'
+    },
+    'Pergunta 7': {
+        'pergunta': 'Qual o nome da noiva cadáver?',
+        'respostas': {'a': 'Sara', 'b': 'Emilia', 'c': 'Emily'},
+        'resposta_certa': 'c'
+    },    
+    'Pergunta 8': {
+        'pergunta': 'Qual o ponto fraco do super-man',
+        'respostas': {'a': 'kryptnonita', 'b': 'gelo', 'c': 'manga'},
+        'resposta_certa': 'a'
+    },
+    'Pergunta 9': {
+        'pergunta': 'Quanto é 2+2?',
+        'respostas': {'a': '5', 'b': '4', 'c': '6'},
+        'resposta_certa': 'b'
+    },
+    'Pergunta 10': {
+        'pergunta': 'Quem é o melhor personagem de carreta furacão',
+        'respostas': {'a': 'superman', 'b': 'batman', 'c': 'fofão'},
+        'resposta_certa': 'c'
+    },
+    
 }
 print()
 # indice_p indice da pergunta, indice_op indice da opçao
